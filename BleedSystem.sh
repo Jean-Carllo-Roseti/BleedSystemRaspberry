6c21fa474f184fa5a5834d363c778ff3b4119736
Jean Carllo <jeanrosete04@gmail.com>
2025-01-24 12:03:38 -0300

    concluido executavel, pronto para testes.

---
+++ BleedSystem.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Inicia o programa Python
+"/home/avionics/Refri/BleedSystemRaspberry/Back-End/dist/todos_modulos" &
+
+# Inicia o programa Processing
+"/home/avionics/Refri/BleedSystemRaspberry/Front-End/linux-aarch64/Front-End" &
