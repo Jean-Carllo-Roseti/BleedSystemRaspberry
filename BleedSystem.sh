f4cf3236c7c0fe6118c2357fa177a2e822d1e173
Jean Carllo <jeanrosete04@gmail.com>
2025-01-24 12:13:10 -0300

    finalizado a sincronia, pronto para testes.

--- BleedSystem.sh
+++ BleedSystem.sh
@@ -5,3 +5,8 @@
 
 # Inicia o programa Processing
 "/home/avionics/Refri/BleedSystemRaspberry/Front-End/linux-aarch64/Front-End" &
+
+# Monitora as alterações nos diretórios e sincroniza automaticamente
+while inotifywait -e modify,move,create,delete /home/avionics/Refri/BleedSystemRaspberry/ScrenShots/Registros; do
+    rsync -avz /home/avionics/Refri/BleedSystemRaspberry/ScrenShots/Registros /home/avionics/Desktop/RegistrosBleedS/
+done
